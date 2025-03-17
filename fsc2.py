444c13ba5e7a094537f5744c378e192aa1d11d44
Akari Mekahime <mekahime.akari@gmail.com>
2025-03-17 19:39:51 +0000

    more stuff

--- fsc2.py
+++ fsc2.py
@@ -8,8 +8,11 @@ import sys
 import codecs
 import copy
 import pprint
-from lark import Lark, Token, Tree
+import logging
+from lark import Lark, Token, Tree, logger
 from lark.visitors import Interpreter
+
+logger.setLevel(logging.DEBUG)
 
 parser = Lark.open('foxscream2.lark', rel_to=__file__)
 print(parser.parse("cheese = \"sjd\\\"k;s\n'd'j\"\n + 2\na;").pretty())
@@ -20,3 +23,14 @@ print(parser.parse("cheese: [int] = fn (a, b): [int] {return a + b}\n").pretty()
 print(parser.parse("ns cheese; nee = ns {cheese = fn (a, b): [int] {return a + b}}\n").pretty())
 print(parser.parse("a: class[cheese] = static class cheese: fries, chicken {private noinherit fn superchiken() { super.class.hello()}}\n").pretty())
 print(parser.parse("a, b, c = fries() \n ").pretty())
+print(parser.parse("""
+class cheesey
+{
+    fries: a
+    chicken: b
+    dineo: as = "friess"
+    a
+    sdsds: asas{[sdhs] has fn (a, b):a and fn (b, a):b}
+    sdsds2: asas[sdhs] has fn (a, b):a, fn (b, a):b = ccheese()
+}
+""").pretty())
